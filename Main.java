920c82ed920f4a31fea9f4e515ec41e0dc4b14ba
tako2039 <takeo2039@gmail.com>
2022-04-02 00:02:14 -0500

    practice more about if and else

--- Main.java
+++ Main.java
@@ -3,11 +3,10 @@ package com.gaho;
 public class Main {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 4900;
-        int levelCompleted = 5;
-        int bonus = 100;
-        System.out.println("Score: " + score);
+        boolean p1GameOver = true;
+        int p1Score = 100;
+        int p1LevelCompleted = 5;
+        int p1Bonus = 100;
 
         // if (score > 5000) {
         //    System.out.println("Your score was equal to or more than 5000.");
@@ -23,8 +22,24 @@ public class Main {
         //     System.out.println("Your score was more than 0 and less than 1000.");
         // } else {
         //     System.out.println("Your score must be a negative number.");
+        // }
+        // System.out.println("This was executed.");
 
+        if (p1GameOver) {
+            int p1FinalScore = p1Score + (p1LevelCompleted * p1Bonus);
+            p1FinalScore += 1000;
+            System.out.println("p1's final score was " + p1FinalScore);
         }
-        System.out.println("This was executed.");
+
+        boolean p2GameOver = true;
+        int p2Score = 10000;
+        int p2LevelCompleted = 8;
+        int p2Bonus = 200;
+
+        if (p2GameOver) {
+            int p2FinalScore = p2Score + (p2LevelCompleted * p2Bonus);
+            p2FinalScore += 1000;
+            System.out.println("p2's final score was " + p2FinalScore);
+        }
     }
 }
