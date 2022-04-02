ab8800e6a9ab301ddb04320083d28689d5018f2c
tako2039 <takeo2039@gmail.com>
2022-04-01 23:26:02 -0500

    practice if else

--- Main.java
+++ Main.java
@@ -4,15 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 5000;
+        int score = 4900;
         int levelCompleted = 5;
         int bonus = 100;
         System.out.println("Score: " + score);
 
-        if (score < 4000) {
-            System.out.println("Your score was less than 5000.");
-        } else {
-            System.out.println("Your score was more or equal to 4000.");
+        // if (score > 5000) {
+        //    System.out.println("Your score was equal to or more than 5000.");
+        // } else if (score > 4000) {
+        //     System.out.println("Your score was more than 4000 and less than 5000.");
+        // } else if (score > 3000) {
+        //     System.out.println("Your score was more than 3000 and less than 4000.");
+        // } else if (score > 2000) {
+        //     System.out.println("Your score was more than 2000 and less than 3000.");
+        // } else if (score > 1000) {
+        //     System.out.println("Your score was more than 1000 and less than 2000.");
+        // } else if (score > 0) {
+        //     System.out.println("Your score was more than 0 and less than 1000.");
+        // } else {
+        //     System.out.println("Your score must be a negative number.");
 
         }
         System.out.println("This was executed.");
